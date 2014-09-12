5877566fc24e3ee8156cbad501f816e7134ccd3f
Joseph Snee <snoey.92@gmail.com>
2014-09-12 16:24:36 -0500

    Added first attempts at the month view tab and the today tab.

--- src/com/example/dmsocialclub/TodayFragment.java
+++ src/com/example/dmsocialclub/TodayFragment.java
@@ -1,7 +1,20 @@
 package com.example.dmsocialclub;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class TodayFragment extends Fragment {
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.activity_main, container, false);
+         
+        return rootView;
+    }
 
 }
