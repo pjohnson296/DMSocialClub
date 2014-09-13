417a20d9ef17242eab7e83d1457856235ccb75ef
Joseph Snee <snoey.92@gmail.com>
2014-09-13 16:05:28 -0500

    Fixed the categories menu dropdown.

--- src/com/example/dmsocialclub/AboutActivity.java
+++ src/com/example/dmsocialclub/AboutActivity.java
@@ -26,6 +26,7 @@ public class AboutActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
+		//return this.getParent().onOptionsItemSelected(item);
 //		if (id == R.id.action_settings) {
 //			return true;
 //		}
